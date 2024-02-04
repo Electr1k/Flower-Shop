c2c8baaf8847d13d1a4fdb84bd41ef03c213792e
Electr1k <gynter37@gmail.com>
2024-02-05 00:54:47 +0300

    fix validation on create

--- resources/views/flower/create.blade.php
+++ resources/views/flower/create.blade.php
@@ -5,18 +5,22 @@
             @csrf
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" name="title" id="title" aria-describedby="Title">
+                <input value="{{ old('title') }}" type="text" class="form-control" name="title" id="title" aria-describedby="Title">
+                @error('title')
+                <p class="text-danger">{{$message }}</p>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <input type="text" class="form-control" name="description" id="description" aria-describedby="description">
+                <input value="{{ old('description') }}" type="text" class="form-control" name="description" id="description" aria-describedby="description">
             </div>
 
             <div class="form-group">
                 <label for="category">Категория</label>
                 <select class="form-control" id="category" name="category_id">
                     @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->title }}</option>
+                        <option {{ old('category_id') == $category->id ? ' selected' : ''}}
+                                value="{{ $category->id }}">{{ $category->title }}</option>
                     @endforeach
                 </select>
             </div>
